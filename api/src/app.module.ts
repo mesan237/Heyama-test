75b5452f7cb9c336df7eb4acd0ba1f57dee4d3ce
mesan237 <negokamgaing@gmail.com>
2026-04-04 18:34:21 +0100

    chore:génerations des objets et mise en place de la structure de ma BD

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ObjectsModule } from './objects/objects.module';
+import { ObjectsModule } from './objects/objects.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true}),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        uri: config.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    ObjectsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
